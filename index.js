7b77182b85c300b293731fc608e512fe7dbe3585
1804011 <sharifsharifulislam061@gmail.com>
2022-05-01 19:30:21 +0600

    add item done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -14,12 +14,21 @@ const client = new MongoClient(uri, {
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-	const collection = client.db("test").collection("devices");
-	// perform actions on the collection object
-	console.log("connected");
-	client.close();
-});
+async function run() {
+	try {
+		await client.connect();
+		app.post("/inventory-items", async (req, res) => {
+			const database = client.db("inventory-items");
+			const itemsCollection = database.collection("items");
+			const result = await itemsCollection.insertOne(req.body);
+			res.send(result);
+		});
+		// create a document to insert
+	} finally {
+		//await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
 	res.send({ id: 8 });
